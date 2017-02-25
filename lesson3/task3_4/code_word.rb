df5ccbc2ba01963e5d4f3672f19a997449532ae8
s.shimko <sergey1shimko@gmail.com>
2017-02-25 23:51:30 +0300

    task3_4 sha2 added

--- lesson3/task3_4/code_word.rb
+++ lesson3/task3_4/code_word.rb
@@ -1,5 +1,4 @@
-require 'digest/md5'
-require 'digest/sha1'
+require 'digest'
 
 puts "Enter word for encode:"
 word = gets.chomp
@@ -7,19 +6,22 @@ word = gets.chomp
 puts "What type of coding?"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 
 code_type = ''
-until [1, 2].include?(code_type) do
+until [1, 2, 3].include?(code_type) do
   code_type = gets.chomp.to_i
-  if [1, 2].include?(code_type) == false
+  if [1, 2, 3].include?(code_type) == false
     puts "Wrong type!"
   end
 end
 
 if code_type == 1
   decode_word = Digest::MD5.hexdigest(word)
-else
+elsif code_type == 2
   decode_word = Digest::SHA1.hexdigest(word)
+else
+  decode_word = Digest::SHA2.hexdigest(word)
 end
 
 
