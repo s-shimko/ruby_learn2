352edae8d0a6f0dc5a494246daa7710caad507d3
s.shimko <sergey1shimko@gmail.com>
2017-04-12 00:50:43 +0300

    task12_2 finished

--- lesson12/task12_1_2/query.rb
+++ lesson12/task12_1_2/query.rb
@@ -2,9 +2,21 @@ require 'sqlite3'
 
 db = SQLite3::Database.open("friends.sqlite")
 
-i = 0
+c_id = db.execute("SELECT id FROM companies WHERE company_name = 'Invento'")
+
 puts "| rowid | first_name | phone | company_id |"
 
-db.execute( "SELECT rowid, first_name, phone, company_id FROM people WHERE company_id = 1" ) do |row|
+db.execute("SELECT rowid, first_name, phone, company_id FROM people WHERE company_id = ?", c_id) do |row|
+  puts "| #{row[0]}     | #{row[1]}      | #{row[2]}   |     #{row[3]}     |"
+end
+
+puts
+puts "OR"
+puts
+
+puts "| rowid | first_name | phone | company_id |"
+
+db.execute("SELECT rowid, first_name, phone, company_id FROM people WHERE company_id = " +
+               "(SELECT id FROM companies WHERE company_name = 'Epam')") do |row|
   puts "| #{row[0]}     | #{row[1]}      | #{row[2]}   |     #{row[3]}     |"
 end
