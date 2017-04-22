eb39d9835832d2ec836e431be818a6f4c8750454
s.shimko <sergey1shimko@gmail.com>
2017-04-22 16:26:47 +0300

    task13_3 currency - their version

--- lesson13/task13_3/currency.rb
+++ lesson13/task13_3/currency.rb
@@ -8,32 +8,39 @@ response = Net::HTTP.get_response(uri)
 
 doc = REXML::Document.new(response.body)
 
-valutes = doc.elements["ValCurs"].elements
+# valutes = doc.elements["ValCurs"].elements
+#
+# def show_currency(valutes, *args)
+#   for i in 0..args.size
+#     valutes.each do |el|
+#       if el.elements["CharCode"].text == args[i]
+#         puts "#{el.elements["Name"].text}: #{el.elements["Value"].text} руб."
+#         puts
+#       end
+#     end
+#   end
+# end
+#
+# puts "What currencies do you wanna know?"
+# puts "'end' to finish "
+# arr = []
+# valutes.each { |el| arr << el.elements["CharCode"].text }
+# puts arr.join(", ")
+# puts
+#
+# choice = nil
+# arr_choice = []
+# while choice != "end"
+#   arr_choice << choice = gets.chomp
+# end
+#
+#
+# show_currency(valutes, *arr_choice)
 
-def show_currency(valutes, *args)
-  for i in 0..args.size
-    valutes.each do |el|
-      if el.elements["CharCode"].text == args[i]
-        puts "#{el.elements["Name"].text}: #{el.elements["Value"].text} руб."
-        puts
-      end
-    end
-  end
+doc.each_element('//Valute[@ID="R01235" or @ID="R01239"]') do |currency_tag|
+  name = currency_tag.get_text('Name')
+  value = currency_tag.get_text('Value')
+
+  # Аккуратно выводим пользователю
+  puts "#{name}: #{value} руб."
 end
-
-puts "What currencies do you wanna know?"
-puts "'end' to finish "
-arr = []
-valutes.each { |el| arr << el.elements["CharCode"].text }
-puts arr.join(", ")
-puts
-
-choice = nil
-arr_choice = []
-while choice != "end"
-  arr_choice << choice = gets.chomp
-end
-
-
-show_currency(valutes, *arr_choice)
-
